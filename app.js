4aa0b960ac057a106f59f463103ab019044be357
Axel Villablanca <axel.villablanca@gmail.com>
2024-08-09 19:25:34 -0400

    Actualizacion Html y css, javascript otalmente funcional

--- app.js
+++ app.js
@@ -2,27 +2,39 @@ console.log("La paleta de colores fue sacada del tomo 14 de Chainsaw Man ;)")
 
 const botonEncriptar = document.getElementById("boton1");
 const botonDesencriptar = document.getElementById("boton2");
-const rawText = document.getElementById("texto")
+const rawText = document.getElementById("texto");
+const excluir = /[A-ZÁÉÍÓÚÑáéíóúñ!@#$%^&*()_+\-=[\]{};':",./<>?\|]/;
+const textoVacio = "";
 
 
+
+
+//Comprobar Mayusculas y simbolos
+function comprobar(textoComprobado){
+    if(excluir.test(textoComprobado)){
+        alert("Texto no Valido");
+        return textoVacio
+    }else{
+        return textoComprobado
+    }
+}
 
 // Encriptador 
 function verificarEncriptar(textarea){
+    if(textarea == textoVacio){
+        return textoVacio
+    }
 
     let textoEncriptado = textarea ;
 
     if(textoEncriptado.includes("e")){
-        textoEncriptado = textoEncriptado.replaceAll("e", 'enter');
-                
+        textoEncriptado = textoEncriptado.replaceAll("e", 'enter');              
     }if(textoEncriptado.includes("i")){
-        textoEncriptado = textoEncriptado.replaceAll("i", 'imes');
-       
+        textoEncriptado = textoEncriptado.replaceAll("i", 'imes');     
     }if(textoEncriptado.includes("a")){
-        textoEncriptado = textoEncriptado.replaceAll("a", 'ai');
-        
+        textoEncriptado = textoEncriptado.replaceAll("a", 'ai');       
     }if(textoEncriptado.includes("o")){
         textoEncriptado = textoEncriptado.replaceAll("o", 'ober');
-        
     }if(textoEncriptado.includes("u")){
         textoEncriptado = textoEncriptado.replaceAll("u", 'ufat');
     }
@@ -32,24 +44,22 @@ function verificarEncriptar(textarea){
 }
 
 
-
-// Encriptador 
+// Desencriptar 
 function verificarDesencriptar(textarea){
+    if(textarea == textoVacio){
+        return textoVacio
+    }
 
     let textoDesencriptado = textarea ;
 
     if(textoDesencriptado.includes("enter")){
-        textoDesencriptado = textoDesencriptado.replaceAll("enter", 'e');
-                
+        textoDesencriptado = textoDesencriptado.replaceAll("enter", 'e');           
     }if(textoDesencriptado.includes("imes")){
-        textoDesencriptado = textoDesencriptado.replaceAll("imes", 'i');
-       
+        textoDesencriptado = textoDesencriptado.replaceAll("imes", 'i');   
     }if(textoDesencriptado.includes("ai")){
-        textoDesencriptado = textoDesencriptado.replaceAll("ai", 'a');
-        
+        textoDesencriptado = textoDesencriptado.replaceAll("ai", 'a');  
     }if(textoDesencriptado.includes("ober")){
-        textoDesencriptado = textoDesencriptado.replaceAll("ober", 'o');
-        
+        textoDesencriptado = textoDesencriptado.replaceAll("ober", 'o');  
     }if(textoDesencriptado.includes("ufat")){
         textoDesencriptado = textoDesencriptado.replaceAll("ufat", 'u');
     }
@@ -59,40 +69,39 @@ function verificarDesencriptar(textarea){
 }
 
 
-
 //Inserta el texto en el panel de la derecha
 function insertar(texto){
+    if(texto == textoVacio){
+        //Si esta vacio no pasa nada
+    }else{
+    document.getElementsByTagName("h2")[0].style.display="none";    
+    document.getElementsByClassName("mensaje")[0].style.display= "none"
+
     const lista = document.querySelector("ul")
     const contenido = document.createElement("li");
     contenido.textContent = texto;
     contenido.classList.add("encriptado");
     lista.appendChild(contenido);
+    }
 }
 
 
 
-
-
-
-
 function encriptar(){
-    document.getElementsByTagName("h2")[0].style.display="none";    
-    let texto = verificarEncriptar(rawText.value);
+    let textoComprobado = comprobar(rawText.value)
+    let texto = verificarEncriptar(textoComprobado);
     
     insertar(texto);
     rawText.value=""
 }
 
 function desencriptar(){
-    document.getElementsByTagName("h2")[0].style.display="none";    
-    let texto = verificarDesencriptar(rawText.value);
+    let textoComprobado = comprobar(rawText.value)
+    let texto = verificarDesencriptar(textoComprobado);
     
     insertar(texto);
     rawText.value=""
 }
-
-
-
 
 
 botonEncriptar.addEventListener("click",encriptar); //boton encriptar
@@ -104,14 +113,6 @@ rawText.addEventListener("keydown", function(e){
     }
 })
 
+//Copiar texto
 
 
-
-
-/**
- * La letra "e" es convertida para "enter"
- * La letra "i" es convertida para "imes"
- * La letra "a" es convertida para "ai"
- * La letra "o" es convertida para "ober"
- * La letra "u" es convertida para "ufat"
- */
